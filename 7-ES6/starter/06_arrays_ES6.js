319d98f1587c439b4f2b256b2809ec06e94c189c
Bruno Tacio <bruno018@gmail.com>
2018-07-07 15:19:17 -0300

    section 7 - Lectures 6, 7, 8

--- 7-ES6/starter/06_arrays_ES6.js
+++ 7-ES6/starter/06_arrays_ES6.js
@@ -8,7 +8,43 @@ const boxes = document.querySelectorAll('.box')
 // })
 
 // ES6
+// console.log('boxes', ...boxes)
 const boxesArr6 = Array.from(boxes)
 boxesArr6.forEach((cur)=>{
   cur.style.backgroundColor = 'steelblue'
 })
+
+
+// LOOPS ES5
+// for(var i = 0; i < boxesArr5.length;i++){
+//   if(boxesArr5[i].className === 'box blue'){
+//     // continue
+//     break
+//   }
+//   boxesArr5[i].textContent = 'I changed to blue'
+// }
+
+// LOOPS ES6
+for(const cur of boxesArr6){
+  if(cur.className.includes('blue')){
+    continue
+  }
+  cur.textContent = 'I changed to blue ES6'
+}
+
+// ES5
+
+var ages = [12, 17, 8, 21, 14, 11]
+
+var full = ages.map(function(cur){
+  return cur > 18
+})
+// console.log(ages)
+// console.log(full) // [false, false, false, true, false, false]
+// console.log(full.indexOf(true)) // 3
+// console.log(ages[full.indexOf(true)]) // 21
+
+// ES6
+fulles6 = ages.findIndex( cur => cur >= 18 )
+console.log(fulles6) // 3
+console.log(ages.find( cur => cur >= 18) ) //21
