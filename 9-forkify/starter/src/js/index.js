92acf1c1824101af5956e49a6cc402d1cd1492ff
Bruno Tacio <bruno018@gmail.com>
2018-08-12 15:44:16 -0300

    Forkify - Done!

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,9 +1,11 @@
 import Search from './models/Search'
 import Recipe from './models/Recipe'
 import List from './models/list'
+import Likes from './models/Likes'
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
 import * as listView from './views/listView'
+import * as likesView from './views/likesView'
 import {elements, renderLoader, clearLoader} from './views/base'
 
 /**Global state of the app
@@ -14,10 +16,14 @@ import {elements, renderLoader, clearLoader} from './views/base'
  */
 
 const state = {}
-window.state = state
+// window.state = state
+
+
 /** 
  * SEARCH CONTROLLER 
 */
+
+
 const controlSearch = async() => {
   // 1) Get query from view
   const query = searchView.getInput()
@@ -91,7 +97,7 @@ const controlRecipe = async () => {
   
       // Render Recipe
       clearLoader()
-      recipeView.renderRecipe(state.recipe)
+      recipeView.renderRecipe(state.recipe, state.likes.isLiked(id))
 
       // console.log(state.recipe)
 
@@ -143,6 +149,66 @@ const controlList = () => {
 
 }
 
+/** 
+ * LIKE CONTROLLER 
+*/
+
+// TESTING
+state.likes = new Likes()
+
+
+const controlLike = () => {
+  if(!state.likes) state.likes = new Likes()
+
+  const currentID = state.recipe.id
+  // Usser has NOT yet liked current recipe
+  if(!state.likes.isLiked(currentID)){
+    // Add like t the state
+    const newLike = state.likes.addLike(
+      currentID,
+      state.recipe.title,
+      state.recipe.author,
+      state.recipe.img,
+    )
+
+    // Toggle the like button
+    likesView.toggleLikeBtn(true)
+    
+    // Add like to UI list
+    likesView.renderLike(newLike)
+    console.log(state.likes)
+    
+    // User HAS liked current recipe
+  }else{
+    // Remove like from the state
+    state.likes.deleteLike(currentID)
+
+    // Toggle the like button
+    likesView.toggleLikeBtn(false)
+    
+    // Remove like from UI list
+    likesView.deletelike(currentID)
+    console.log(state.likes)
+
+  }
+  likesView.toggleLikeMenu(state.likes.getNumLikes())
+}
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+  state.likes = new Likes()
+
+  // Restore likes
+  state.likes.readStorage()
+
+  // Toggle like menu button
+  likesView.toggleLikeMenu(state.likes.getNumLikes())
+
+  // Render the existing likes
+  state.likes.likes.forEach(like => likesView.renderLike(like))
+})
+
+
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
   if(e.target.matches('.btn-decrease, .btn-decrease *')){
@@ -157,7 +223,11 @@ elements.recipe.addEventListener('click', e => {
     recipeView.updateServingsIngredients(state.recipe)
   }else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
     listView.clearList(state.list)
+    // Add ingredients to shopping list
     controlList()
+  }else if(e.target.matches('.recipe__love, .recipe__love *')){
+    // Like Controller
+    controlLike()
   }
   // console.log(state.recipe)
 })
